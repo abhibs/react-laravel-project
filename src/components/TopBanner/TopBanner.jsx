c5d9cd68130e7823553e0d2e777a4c7bcd3bc949
abhibs <abhirambs97@gmail.com>
2025-02-08 18:11:10 +0530

    bootstrap min css file added and working banner content

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Button, Col } from 'react-bootstrap'
 import '../../assets/css/custom.css'
+import '../../assets/css/bootstrap.min.css'
 
 
 class TopBanner extends Component {
   render() {
     return (
         <>
-            <Container className="topFixedBanner">
-                                
+            <Container fluid={true} className="topFixedBanner">
+                <div className="topBannerOverlay">
+                    <Container className="topContent">
+                        <Row>
+                            <Col className="text-center">
+                                <h1>Abhiram B S</h1>
+                                <h4>Learn Profesionally</h4>
+                                <Button variant="primary">Learn More</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </Container>
         </>
     )
