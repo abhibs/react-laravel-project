18990d7c29c8d0e3cdf0ce886b13c33635bc8350
abhibs <abhirambs97@gmail.com>
2025-02-08 17:52:00 +0530

    css through add image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import TopBanner from './components/TopBanner/TopBanner';
 
 
 function App() {
   return (
     <div>
-
+      <TopBanner />
     </div>
   );
 }
--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Container } from 'react-bootstrap'
+import '../../assets/css/custom.css'
+
 
 class TopBanner extends Component {
   render() {
     return (
         <>
-            <Container>
+            <Container className="topFixedBanner">
                                 
             </Container>
         </>
