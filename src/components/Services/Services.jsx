018f17116c567abce6c68a0e14bb28fd3cb3a3a4
abhibs <abhirambs97@gmail.com>
2025-02-11 12:51:55 +0530

    service component heading and paragraph design

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -13,24 +13,24 @@ class Services extends Component {
             <Container>
                 <Row>
                     <Col lg={4} md={6} sm={12}>
-                        <div>
+                        <div className="serviceCard text-center">
                             <img className="ecommerceIcon" src={ecommerceIcon} alt="" />
-                            <h2>Ecommerce</h2>
-                            <p>I will design and develop ecommerce online store website.</p>
+                            <h2 className="serviceName">Ecommerce</h2>
+                            <p className="serviceDescription">I will design and develop ecommerce online store website.</p>
                         </div>
                     </Col>
                     <Col lg={4} md={6} sm={12}>
-                        <div>
+                        <div className="serviceCard text-center">
                             <img className="designIcon" src={designIcon} alt="" />
-                            <h2>Web Design</h2>
-                            <p>Qualified web design and attractive effects which catches visitor’s Eye.</p>
+                            <h2 className="serviceName">Web Design</h2>
+                            <p className="serviceDescription">Qualified web design and attractive effects which catches visitor’s Eye.</p>
                         </div>
                     </Col>
                     <Col lg={4} md={6} sm={12}>
-                        <div>
+                        <div className="serviceCard text-center">
                             <img className="webIcon" src={webIcon} alt="" />
-                            <h2>Web Development</h2>
-                            <p>Clean and fresh issue free code to make your website dynamic perfectly.</p>
+                            <h2 className="serviceName">Web Development</h2>
+                            <p className="serviceDescription">Clean and fresh issue free code to make your website dynamic perfectly.</p>
                         </div>
                     </Col>
                 </Row>
