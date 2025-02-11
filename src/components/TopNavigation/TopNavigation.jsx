abdaa3b4719c22e0728a5326ad057c6bb3b63259
abhibs <abhirambs97@gmail.com>
2025-02-11 14:47:46 +0530

    top navigation component created

---
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+class TopNavigation extends Component {
+  render() {
+    return (
+      <>
+        <Navbar
+          collapseOnSelect
+          fixed="top"
+          expand="lg"
+          bg="dark"
+          variant="dark"
+        >
+          <Navbar.Brand href="#home">EASY LEARNING</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto"></Nav>
+            <Nav>
+              <Nav.Link href="#deets">HOME</Nav.Link>
+              <Nav.Link href="#deets">ABOUT</Nav.Link>
+              <Nav.Link href="#deets">SERVICE</Nav.Link>
+              <Nav.Link href="#deets">COURSES</Nav.Link>
+              <Nav.Link href="#deets">PORTFOLIO</Nav.Link>
+              <Nav.Link href="#deets">CONTACT US</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
+    );
+  }
+}
+
+export default TopNavigation;
