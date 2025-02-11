f32ddd0b7dc7694863f1fd9629cca75cdff3d4bd
abhibs <abhirambs97@gmail.com>
2025-02-11 11:06:57 +0530

    heading and paragraph add in service component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,6 +3,8 @@ import { Col, Container, Row } from 'react-bootstrap'
 import designIcon from '../../assets/images/design.png';
 import ecommerceIcon from '../../assets/images/ecommerce.png';
 import webIcon from '../../assets/images/web.png';
+import '../../assets/css/custom.css'
+import '../../assets/css/bootstrap.min.css'
 
 class Services extends Component {
   render() {
@@ -13,16 +15,22 @@ class Services extends Component {
                     <Col lg={4} md={6} sm={12}>
                         <div>
                             <img className="ecommerceIcon" src={ecommerceIcon} alt="" />
+                            <h2>Ecommerce</h2>
+                            <p>I will design and develop ecommerce online store website.</p>
                         </div>
                     </Col>
                     <Col lg={4} md={6} sm={12}>
                         <div>
                             <img className="designIcon" src={designIcon} alt="" />
+                            <h2>Web Design</h2>
+                            <p>Qualified web design and attractive effects which catches visitor’s Eye.</p>
                         </div>
                     </Col>
                     <Col lg={4} md={6} sm={12}>
                         <div>
                             <img className="webIcon" src={webIcon} alt="" />
+                            <h2>Web Development</h2>
+                            <p>Clean and fresh issue free code to make your website dynamic perfectly.</p>
                         </div>
                     </Col>
                 </Row>
