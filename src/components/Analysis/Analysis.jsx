6194ba12429eb71eb9c4fb858c3f80c55a844842
abhibs <abhirambs97@gmail.com>
2025-02-11 16:38:57 +0530

    dummy content add to ananysis component

--- src/components/Analysis/Analysis.jsx
+++ src/components/Analysis/Analysis.jsx
@@ -13,7 +13,28 @@ class Analysis extends Component {
               <h1>Bar Chart</h1>
             </Col>
             <Col lg={6} md={12} sm={12}>
-              <h1>Text</h1>
+              <p className="text-justify serviceDescription">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum
+                libero placeat tenetur consectetur omnis dolore quia cupiditate
+                accusamus vitae nihil. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Nihil, debitis delectus placeat inventore
+                expedita dolorum dolor amet error fuga nesciunt?
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+                excepturi debitis ratione quas praesentium odio. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Placeat debitis
+                ipsum culpa cupiditate magnam commodi eius tenetur error laborum
+                est!
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Maiores numquam ipsum nisi, tempore molestiae quibusdam beatae,
+                harum tempora praesentium nesciunt error labore aliquid
+                temporibus maxime. Lorem, ipsum dolor sit amet consectetur
+                adipisicing elit. Quasi facilis rem minus quaerat velit esse
+                possimus aut, molestias non repudiandae.
+              </p>
             </Col>
           </Row>
         </Container>
