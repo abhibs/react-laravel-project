855fecb96644c30c3d044b65a8b9e5070a1bf276
abhibs <abhirambs97@gmail.com>
2025-02-12 16:14:05 +0530

    Recent Project Component Created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Services from "./components/Services/Services";
 import TopNavigation from "./components/TopNavigation/TopNavigation";
 import Analysis from "./components/Analysis/Analysis";
 import Summury from "./components/Summary/Summury";
+import RecentProject from "./components/RecentProject/RecentProject";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Services />
       <Analysis />
       <Summury />
+      <RecentProject />
     </div>
   );
 }
---
+++ src/components/RecentProject/RecentProject.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class RecentProject extends Component {
+  render() {
+    return (
+      <>
+        <h1>Recent Projects</h1>
+      </>
+    );
+  }
+}
+
+export default RecentProject;
