1836c6c474467c3a3959a874b26a4baca15df0fb
abhibs <abhirambs97@gmail.com>
2025-02-12 12:38:42 +0530

    summery component design

--- src/components/Summary/Summury.jsx
+++ src/components/Summary/Summury.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 class Summury extends Component {
   render() {
     return (
       <>
-        <h1>Summary Component</h1>
+        <Container fluid={true} className="summaryBanner">
+          <Row>
+            <Col lg={8} md={6} sm={12}>
+              <Row>
+                <Col>
+                  <h1>One</h1>
+                </Col>
+                <Col>
+                  <h1>Two</h1>
+                </Col>
+                <Col>
+                  <h1>Three</h1>
+                </Col>
+              </Row>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <h1>Four</h1>
+            </Col>
+          </Row>
+        </Container>
       </>
     );
   }
