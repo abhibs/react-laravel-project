2b39f5c427ef0aa13a9e942553c90aa1f2daac39
abhibs <abhirambs97@gmail.com>
2025-02-12 16:30:17 +0530

    responsive design of Recent Project Component

--- src/components/RecentProject/RecentProject.jsx
+++ src/components/RecentProject/RecentProject.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 class RecentProject extends Component {
   render() {
     return (
       <>
-        <h1>Recent Projects</h1>
+        <Container>
+          <Row>
+            <Col lg={4} md={6} sm={12}>
+              <h1>One</h1>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <h1>Two</h1>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <h1>Three</h1>
+            </Col>
+          </Row>
+        </Container>
       </>
     );
   }
