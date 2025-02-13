f6b57a78b902b08f1f437aa20f497ff63a3a9c86
abhibs <abhirambs97@gmail.com>
2025-02-13 15:15:41 +0530

    course component design completed

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,10 +1,90 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import course1 from "../../assets/images/course1.avif";
+import course2 from "../../assets/images/course2.avif";
+import course3 from "../../assets/images/course3.avif";
+import course4 from "../../assets/images/course4.avif";
 
 export class Courses extends Component {
   render() {
     return (
       <>
-        <h1>Course Component</h1>
+        <Container className="text-center">
+          <h1 className="serviceMainTitle">MY COURSES</h1>
+          <div className="bottom"></div>
+          <Row className="mb-3">
+            <Col lg={6} md={12} sm={12}>
+              <Row>
+                <Col lg={6} md={6} sm={12}>
+                  <img className="courseImg" src={course1} />
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                  <h5 className="text-justify serviceName">Laravel 8 </h5>
+                  <p className="text-justify serviceDescription">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a className="courseViewMore float-left" href="#">
+                    View Details
+                  </a>
+                </Col>
+              </Row>
+            </Col>
+            <Col lg={6} md={12} sm={12}>
+              <Row>
+                <Col lg={6} md={6} sm={12}>
+                  <img className="courseImg" src={course2} />
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                  <h5 className="text-justify serviceName">Laravel 8 </h5>
+                  <p className="text-justify serviceDescription">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a className="courseViewMore float-left" href="#">
+                    View Details
+                  </a>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={6} md={12} sm={12}>
+              <Row>
+                <Col lg={6} md={6} sm={12}>
+                  <img className="courseImg" src={course3} />
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                  <h5 className="text-justify serviceName">Laravel 8 </h5>
+                  <p className="text-justify serviceDescription">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a className="courseViewMore float-left" href="#">
+                    View Details
+                  </a>
+                </Col>
+              </Row>
+            </Col>
+            <Col lg={6} md={12} sm={12}>
+              <Row>
+                <Col lg={6} md={6} sm={12}>
+                  <img className="courseImg" src={course4} />
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                  <h5 className="text-justify serviceName">Laravel 8 </h5>
+                  <p className="text-justify serviceDescription">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a className="courseViewMore float-left" href="#">
+                    View Details
+                  </a>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
       </>
     );
   }
