ac5687eecba0d3539fcd7b1112a6b32635675c62
abhibs <abhirambs97@gmail.com>
2025-02-14 15:40:29 +0530

    face image add to about me component

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import face from "../../assets/images/face.png";
 
 class AboutMe extends Component {
   render() {
     return (
       <>
-        <h1>About Me Component</h1>
+        <Container className="text-center">
+          <h1 className="serviceMainTitle">ABOUT ME</h1>
+          <div className="bottom"></div>
+          <Row>
+            <Col lg={6} md={6} sm={12}>
+              <div className="aboutMeImage">
+                <img className="aboutImg" src={face} />
+              </div>
+            </Col>
+            <Col lg={6} md={6} sm={12}></Col>
+          </Row>
+        </Container>
       </>
     );
   }
