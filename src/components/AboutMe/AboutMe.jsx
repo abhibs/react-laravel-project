8782733a1e03371de1354367cd5f0eafe04f472d
abhibs <abhirambs97@gmail.com>
2025-02-14 17:56:30 +0530

    about us component content created

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -15,7 +15,15 @@ class AboutMe extends Component {
                 <img className="aboutImg" src={face} />
               </div>
             </Col>
-            <Col lg={6} md={6} sm={12}></Col>
+            <Col lg={6} md={6} sm={12}>
+              <div className="aboutMeBody">
+                <h2 className="aboutMeDetails">HI There, I'm</h2>
+                <h2 className="aboutMeTitle">Abhiram</h2>
+                <h3 className="aboutMeDetails">
+                  Work as <span id="myElement"> Full Stack Web Developer </span>
+                </h3>
+              </div>
+            </Col>
           </Row>
         </Container>
       </>
