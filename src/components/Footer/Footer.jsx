d61c66dd22e3e7d8febd9464e94eea6eff84eb3d
abhibs <abhirambs97@gmail.com>
2025-02-15 15:43:42 +0530

    footer component addresss column

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 class Footer extends Component {
   render() {
@@ -26,8 +28,16 @@ class Footer extends Component {
               </div>
             </Col>
 
-            <Col lg="3" md={6} sm={12}>
+            <Col lg="3" md={6} sm={12} className="p-5 text-justify">
               <h2 className="footerName text-center">Address </h2>
+              <p className="footerDescription">
+                Javalli Tudoor Thirthahalli Shimoga Karnataka 577226
+                <br></br>
+                <FontAwesomeIcon icon={faEnvelope} /> Email :
+                abhirambs97@gmail.com
+                <br></br>
+                <FontAwesomeIcon icon={faPhone} /> Phone : 948117822<br></br>
+              </p>
             </Col>
             <Col lg="3" md={6} sm={12}>
               <h2 className="footerName text-center">Information </h2>
