52451e682e1011225acd665da66d8ab2f6a4c2e2
abhibs <abhirambs97@gmail.com>
2025-02-19 12:18:37 +0530

    About me and footer component call in app js file

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import PageTop from "./components/PageTop/PageTop";
 import Homepage from "./pages/Homepage";
 import TopNavigation from "./components/TopNavigation/TopNavigation";
 import AboutDescription from "./components/AboutDescription/AboutDescription";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       {/* <Homepage /> */}
       <TopNavigation />
-      <PageTop />
+      <PageTop pagetitle="About Us" />
+      <AboutMe />
       <AboutDescription />
+      <Footer />
     </div>
   );
 }
