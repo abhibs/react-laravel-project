50cdc1fa6f44ee6a2592641d6a944eec5f9cf048
abhibs <abhirambs97@gmail.com>
2025-02-19 13:13:30 +0530

    about page created and call in app js file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
-import PageTop from "./components/PageTop/PageTop";
-import Homepage from "./pages/Homepage";
-import TopNavigation from "./components/TopNavigation/TopNavigation";
-import AboutDescription from "./components/AboutDescription/AboutDescription";
-import AboutMe from "./components/AboutMe/AboutMe";
-import Footer from "./components/Footer/Footer";
+import Aboutpage from "./pages/Aboutpage";
 
 function App() {
   return (
     <div>
-      {/* <Homepage /> */}
-      <TopNavigation />
-      <PageTop pagetitle="About Us" />
-      <AboutMe />
-      <AboutDescription />
-      <Footer />
+      <Aboutpage />
     </div>
   );
 }
---
+++ src/pages/Aboutpage.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import TopNavigation from "../components/TopNavigation/TopNavigation";
+import PageTop from "../components/PageTop/PageTop";
+import AboutMe from "../components/AboutMe/AboutMe";
+import AboutDescription from "../components/AboutDescription/AboutDescription";
+import Footer from "../components/Footer/Footer";
+
+class Aboutpage extends Component {
+  render() {
+    return (
+      <>
+        <TopNavigation />
+        <PageTop pagetitle="About Us" />
+        <AboutMe />
+        <AboutDescription />
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default Aboutpage;
