1e6fe569cb359efe4b346e42e55f7b74ea94dc54
abhibs <abhirambs97@gmail.com>
2025-02-20 12:20:18 +0530

    service page created

---
+++ src/pages/Services.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Services extends Component {
+  render() {
+    return (
+      <>
+        <h1>Service Page</h1>
+      </>
+    );
+  }
+}
+
+export default Services;
