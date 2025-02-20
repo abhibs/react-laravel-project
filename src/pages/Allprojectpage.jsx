456df8b90b15e49dbf35d0f86e56ce2f8e994653
abhibs <abhirambs97@gmail.com>
2025-02-20 12:10:17 +0530

    app js component transfer to all project page file

--- src/pages/Allprojectpage.jsx
+++ src/pages/Allprojectpage.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import TopNavigation from "../components/TopNavigation/TopNavigation";
+import PageTop from "../components/PageTop/PageTop";
+import AllProjects from "../components/AllProjects/AllProjects";
+import Footer from "../components/Footer/Footer";
 
 class Allprojectpage extends Component {
   render() {
     return (
       <>
-        <h1>All Project Page</h1>
+        <TopNavigation />
+        <PageTop pagetitle="All Project" />
+        <AllProjects />
+        <Footer />
       </>
     );
   }
