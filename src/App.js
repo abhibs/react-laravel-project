f46cf6464bfc6d5f2ed7e0f0907b6f5c96e3e6c3
abhibs <abhirambs97@gmail.com>
2025-02-20 15:43:26 +0530

    basic design of contact component in app js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ContactSec from "./components/ContactSec/ContactSec";
+import PageTop from "./components/PageTop/PageTop";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
+      <TopNavigation />
+      <PageTop pagetitle="Contact Us" />
       <ContactSec />
+      <Footer />
     </div>
   );
 }
--- src/components/ContactSec/ContactSec.jsx
+++ src/components/ContactSec/ContactSec.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 class ContactSec extends Component {
   render() {
     return (
       <>
-        <h1>Contact Component</h1>
+        <Container>
+          <Row>
+            <Col lg={6} md={6} sm={12}>
+              <h1>Quick Connect</h1>
+            </Col>
+            <Col lg={6} md={6} sm={12}>
+              <h1>Discuss Now</h1>
+            </Col>
+          </Row>
+        </Container>
       </>
     );
   }
