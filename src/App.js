e3a3c3228c113ed0ec963bc2b4ea5fc689a83fd3
abhibs <abhirambs97@gmail.com>
2025-02-20 11:15:38 +0530

    app js component transfer to all course page file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
 import Allcoursespage from "./pages/Allcoursespage";
-// import TopNavigation from "./components/TopNavigation/TopNavigation";
-// import AllCourses from "./components/AllCourses/AllCourses";
-// import PageTop from "./components/PageTop/PageTop";
-// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
-      {/* <TopNavigation />
-      <PageTop pagetitle="All Courses" />
-      <AllCourses />
-      <Footer /> */}
-
       <Allcoursespage />
     </div>
   );
--- src/pages/Allcoursespage.jsx
+++ src/pages/Allcoursespage.jsx
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import TopNavigation from "../components/TopNavigation/TopNavigation";
+import PageTop from "../components/PageTop/PageTop";
+import AllCourses from "../components/AllCourses/AllCourses";
+import Footer from "../components/Footer/Footer";
 
 class Allcoursespage extends Component {
   render() {
     return (
-        <>
-            <h1>All Course Page</h1>
-        </>
-    )
+      <>
+        <TopNavigation />
+        <PageTop pagetitle="All Courses" />
+        <AllCourses />
+        <Footer />
+      </>
+    );
   }
 }
 
-export default Allcoursespage
+export default Allcoursespage;
