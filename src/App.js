6f0f5f73aee4e68ab098c28223313fa7c3f8693e
abhibs <abhirambs97@gmail.com>
2025-02-20 11:06:32 +0530

    all course page created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import TopNavigation from "./components/TopNavigation/TopNavigation";
-import AllCourses from "./components/AllCourses/AllCourses";
-import PageTop from "./components/PageTop/PageTop";
-import Footer from "./components/Footer/Footer";
+import Allcoursespage from "./pages/Allcoursespage";
+// import TopNavigation from "./components/TopNavigation/TopNavigation";
+// import AllCourses from "./components/AllCourses/AllCourses";
+// import PageTop from "./components/PageTop/PageTop";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
-      <TopNavigation />
+      {/* <TopNavigation />
       <PageTop pagetitle="All Courses" />
       <AllCourses />
-      <Footer />
+      <Footer /> */}
+
+      <Allcoursespage />
     </div>
   );
 }
---
+++ src/pages/Allcoursespage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Allcoursespage extends Component {
+  render() {
+    return (
+        <>
+            <h1>All Course Page</h1>
+        </>
+    )
+  }
+}
+
+export default Allcoursespage
