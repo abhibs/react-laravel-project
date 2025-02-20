85a052094146fa123de5884756c9d2736984d19a
abhibs <abhirambs97@gmail.com>
2025-02-20 11:54:32 +0530

    all project component design

--- src/components/AllProjects/AllProjects.jsx
+++ src/components/AllProjects/AllProjects.jsx
@@ -1,10 +1,58 @@
 import React, { Component } from "react";
+import { Card, Col, Container, Button, Row } from "react-bootstrap";
+import projectOne from "../../assets/images/project1.avif";
+import projectTwo from "../../assets/images/project2.avif";
+import projectThree from "../../assets/images/project3.avif";
 
 class AllProjects extends Component {
   render() {
     return (
       <>
-        <h1>All Project Component</h1>
+        <Container className="text-center">
+          <h1 className="serviceMainTitle">MY Project</h1>
+          <div className="bottom"></div>
+          <Row>
+            <Col lg={4} md={6} sm={12}>
+              <Card className="projectCard">
+                <Card.Img variant="top" src={projectOne} />
+                <Card.Body>
+                  <Card.Title className="serviceName">Card Title</Card.Title>
+                  <Card.Text className="serviceDescription">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <Card className="projectCard">
+                <Card.Img variant="top" src={projectTwo} />
+                <Card.Body>
+                  <Card.Title className="serviceName">Card Title</Card.Title>
+                  <Card.Text className="serviceDescription">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg={4} md={6} sm={12}>
+              <Card className="projectCard">
+                <Card.Img variant="top" src={projectThree} />
+                <Card.Body>
+                  <Card.Title className="serviceName">Card Title</Card.Title>
+                  <Card.Text className="serviceDescription">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </>
     );
   }
