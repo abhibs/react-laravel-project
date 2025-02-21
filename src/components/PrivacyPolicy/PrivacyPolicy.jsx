3229911a9e47986b2acfa4533d0e4955195372c4
abhibs <abhirambs97@gmail.com>
2025-02-21 12:23:57 +0530

    privacy policy component created

---
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PrivacyPolicy extends Component {
+  render() {
+    return (
+      <>
+        <h1>Privacy Policy Component Created</h1>
+      </>
+    );
+  }
+}
+
+export default PrivacyPolicy;
